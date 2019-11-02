edb23512e4261a3d6ac4ac18cc3924e3eede5c55
MBHayes00 <mbh9947@rit.edu>
2019-11-01 21:45:03 -0400

    Collection UI
    Created a basic template for the UI to display what has been collected.

--- RDGCJameGame/Assets/Scripts/UI/InterfaceManager.cs
+++ RDGCJameGame/Assets/Scripts/UI/InterfaceManager.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class InterfaceManager : MonoBehaviour
 {
+    //Variables
+    [SerializeField]
+    private GameObject[] collectionImages;
+    public bool[] collectTest;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +19,38 @@ public class InterfaceManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        DisplayCollection();
     }
 
+    /// <summary>
+    /// Plays the game 
+    /// </summary>
     public void Play()
     {
         SceneManager.LoadScene(1);
+    }
+
+    /// <summary>
+    /// Displays the collectables that have been collected
+    /// </summary>
+    void DisplayCollection()
+    {
+        //Displays first collectable
+        if (collectTest[0])
+            collectionImages[0].SetActive(true);
+
+        //Displays second collectable
+        if (collectTest[1])
+            collectionImages[1].SetActive(true);
+
+        //Displays third collectable
+        if (collectTest[2])
+            collectionImages[2].SetActive(true);
+
+        //Display fourth collectable
+        if (collectTest[3])
+            collectionImages[3].SetActive(true);
+
+
     }
 }
